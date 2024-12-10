d107246955d20eb0b5c1bd92f9507a5d80b0586c
moweeece <mjespinosa990@gmail.com>
2024-12-09 18:41:16 -0800

    Update 1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// COMSC-210 | Mauricio Espinosa | Final Part 1
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+struct Customer {
+    string name;
+    string order;
+};
+
+
+int main() {
+
+    vector<string> names = {"Mauricio", "Mochi", "Danielle", "Patricia", "John", "Matt"};
+    vector<string> drinks = {"Latte", "Mocha", "Hot Chocolate", "Espresso", "Soda", "Flat White"};
+
+
+
+    return 0;
+}
